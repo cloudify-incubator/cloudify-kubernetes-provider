d23549d9dc77ef19ef68cfaf5853aa935651dcc9
Denis Pauk <pauk.denis@gmail.com>
2017-08-28 17:17:21 +0300

    Cleanup repository

--- kubenetes-test.sh
+++ kubenetes-test.sh
@@ -1,12 +1,8 @@
 # Based on https://github.com/kubernetes/community/blob/master/contributors/devel/development.md
 
-mkdir -p kubenetes_checks
-cd kubenetes_checks
 export GOBIN=`pwd`/bin
 export PATH=$PATH:`pwd`/bin
 export GOPATH=`pwd`
-
-go get -u github.com/tools/godep
 
 # If your GOPATH has multiple paths, pick
 # just one and use it instead of $GOPATH here.
@@ -35,23 +31,9 @@ if [ ! -d "kubernetes" ]; then
 	git remote -v
 else
 	cd kubernetes
-	git pull
+	make clean
+	git checkout master &&	git pull && git checkout v1.7.4
 fi
 
 cd $working_dir/kubernetes
 make GOGCFLAGS="-N -l"
-
-# no arm
-# make cross
-
-hack/install-etcd.sh
-
-export PATH=${GOPATH}/src/k8s.io/kubernetes/third_party/etcd:${PATH}
-
-cd $working_dir/kubernetes
-
-# Run all the presubmission verification. Then, run a specific update script (hack/update-*.sh)
-# for each failed verification. For example:
-#   hack/update-gofmt.sh (to make sure all files are correctly formatted, usually needed when you add new files)
-#   hack/update-bazel.sh (to update bazel build related files, usually needed when you add or remove imports)
-make verify
