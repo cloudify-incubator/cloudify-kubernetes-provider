ddc81134055eaf33ef9c9507969161a4021f0b3a
Denis Pauk <pauk.denis@gmail.com>
2017-09-11 18:28:00 +0300

    Add additional service on manager node

--- examples/blueprint/scripts/kubernetes-master-config.sh
+++ examples/blueprint/scripts/kubernetes-master-config.sh
@@ -8,8 +8,8 @@ ctx logger info "Token $TOKEN"
 
 ctx logger info "Reload kubeadm"
 sed -i 's|admission-control=Initializers,NamespaceLifecycle,LimitRanger,ServiceAccount,PersistentVolumeLabel,DefaultStorageClass,DefaultTolerationSeconds,NodeRestriction,ResourceQuota|admission-control=Initializers,NamespaceLifecycle,LimitRanger,ServiceAccount,DefaultStorageClass,DefaultTolerationSeconds,NodeRestriction,ResourceQuota|g' /etc/kubernetes/manifests/kube-apiserver.yaml
-systemctl daemon-reload
-systemctl stop kubelet && systemctl start kubelet
+sudo systemctl daemon-reload
+sudo systemctl stop kubelet && sudo systemctl start kubelet
 
 ctx logger info "Copy config"
 mkdir -p $HOME/.kube
@@ -33,3 +33,25 @@ EOF
 
 ctx logger info "Download cfy manager"
 ctx download-resource bins/cfy-kubernetes '@{"target_path": "/tmp/cfy-kubernetes"}'
+
+ctx logger info "Install"
+cp /tmp/cfy-kubernetes /usr/bin/cfy-kubernetes
+sudo chmod 555 /usr/bin/cfy-kubernetes
+sudo chown root:root /usr/bin/cfy-kubernetes
+
+ctx logger info "Create service"
+sudo tee /etc/systemd/system/cfy-kubernetes.service <<EOF
+[Unit]
+Description=cfy kubernetes
+
+[Service]
+ExecStart=/usr/bin/cfy-kubernetes --kubeconfig $HOME/.kube/config --cloud-config $HOME/cfy.json
+KillMode=process
+Restart=on-failure
+RestartSec=60s
+EOF
+
+ctx logger info "Start service"
+sudo systemctl daemon-reload
+sudo systemctl enable cfy-kubernetes.service
+sudo systemctl start cfy-kubernetes.service
--- examples/blueprint/scripts/kubernetes-uninstall.sh
+++ examples/blueprint/scripts/kubernetes-uninstall.sh
@@ -3,3 +3,5 @@ sudo kubeadm reset || ctx logger info "No old configurations"
 sudo systemctl stop kubelet || ctx logger info "You dont have kubernetes? wait several moments"
 sudo yum remove -y -q kubelet kubeadm || ctx logger info "No kubernetes yet"
 sudo rm -f /etc/yum.repos.d/kubernetes.repo
+sudo systemctl stop cfy-kubernetes.service
+sudo rm -f /etc/systemd/system/cfy-kubernetes.service
