87083108d0034f54fc781713d81d0d43fb6974d0
Denis Pauk <pauk.denis@gmail.com>
2017-09-11 14:36:23 +0300

    Add export config to blueprint

---
+++ examples/blueprint/scripts/export_config.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import os
+from cloudify import ctx
+try:
+    import yaml
+except ImportError:
+    ctx.logger.error("Need to install yaml package")
+    import pip
+    pip.main(['install', 'pyyaml'])
+    import yaml
+
+if __name__ == '__main__':
+
+    admin_file_dest = os.path.join(os.path.expanduser('~'), '.kube/config')
+
+    with open(admin_file_dest, 'r') as outfile:
+        try:
+            kubernetes_master_config = yaml.load(outfile)
+        except yaml.YAMLError as e:
+            RecoverableError(
+                'Unable to read Kubernetes Admin file: {0}: {1}'.format(
+                    admin_file_dest, str(e)))
+
+    ctx.instance.runtime_properties['configuration_file_content'] = \
+        kubernetes_master_config
--- examples/blueprint/scripts/kubernetes-uninstall.sh
+++ examples/blueprint/scripts/kubernetes-uninstall.sh
@@ -1,3 +1,4 @@
+rm -rf $HOME/.kube || ctx logger info "No old user configuration"
 sudo kubeadm reset || ctx logger info "No old configurations"
 sudo systemctl stop kubelet || ctx logger info "You dont have kubernetes? wait several moments"
 sudo yum remove -y -q kubelet kubeadm || ctx logger info "No kubernetes yet"
