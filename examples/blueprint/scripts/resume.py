e153edfbf659fa21f14c79c4c1e9a52933e19131
Denis Pauk <pauk.denis@gmail.com>
2017-10-17 17:18:00 +0300

    Create types for docker/kubernetes/kubernetes_host node

--- examples/blueprint/scripts/resume.py
+++
@@ -1,4 +0,0 @@
-from cloudify import ctx
-
-resource_id = ctx.node.properties.get('resource_id')
-ctx.logger.info('resume server_id={}'.format(resource_id))
--- examples/blueprint/scripts/suspend.py
+++
@@ -1,4 +0,0 @@
-from cloudify import ctx
-
-resource_id = ctx.node.properties.get('resource_id')
-ctx.logger.info('suspend server_id={}'.format(resource_id))
