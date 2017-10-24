582f6a875aebec20545e1c070183e767e60130ac
earthmant <trammell@cloudify.co>
2017-10-24 14:52:19 +0300

    use plugin to create monitoring

--- examples/cluster_blueprint/scripts/kubernetes-master-status.sh
+++ examples/cluster_blueprint/scripts/kubernetes-master-status.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 for retry_count in {1..10}
 do
 	notreadycount=`kubectl get nodes  |grep -v "STATUS" | grep "NotReady" | wc -l`
@@ -13,27 +15,27 @@ done
 state=`kubectl get nodes`
 ctx logger info "Nodes: ${state}"
 
-ctx logger info "Preparing monitoring services."
-for service_name in "influxdb/influxdb" "influxdb/heapster" "rbac/heapster-rbac"
-do
-	ctx logger info "Start ${service_name}"
-	for retry_count in {1..10}
-	do
-		kubectl create -f https://raw.githubusercontent.com/kubernetes/heapster/release-1.4/deploy/kube-config/${service_name}.yaml && break
-		ctx logger info "Issues with start ${service_name}"
-		sleep 10
-	done
-done
+# ctx logger info "Preparing monitoring services."
+# for service_name in "influxdb/influxdb" "influxdb/heapster" "rbac/heapster-rbac"
+# do
+# 	ctx logger info "Start ${service_name}"
+# 	for retry_count in {1..10}
+# 	do
+# 		kubectl create -f https://raw.githubusercontent.com/kubernetes/heapster/release-1.4/deploy/kube-config/${service_name}.yaml && break
+# 		ctx logger info "Issues with start ${service_name}"
+# 		sleep 10
+# 	done
+# done
 
-ctx logger info "Check state monitoring services."
-for retry_count in {1..10}
-do
-	notreadycount=`kubectl get pods --namespace=kube-system | awk '{print $3}' | grep -v "Running" | grep -v "STATUS" | wc -l`
-	ctx logger info "#${retry_count}: ${notreadycount} pods are NotRunning"
-	if [ "z$notreadycount" == 'z0' ]; then
-		break
-	else
-		ctx logger info "Wait little more."
-		sleep 10
-	fi
-done
+# ctx logger info "Check state monitoring services."
+# for retry_count in {1..10}
+# do
+# 	notreadycount=`kubectl get pods --namespace=kube-system | awk '{print $3}' | grep -v "Running" | grep -v "STATUS" | wc -l`
+# 	ctx logger info "#${retry_count}: ${notreadycount} pods are NotRunning"
+# 	if [ "z$notreadycount" == 'z0' ]; then
+# 		break
+# 	else
+# 		ctx logger info "Wait little more."
+# 		sleep 10
+# 	fi
+# done
