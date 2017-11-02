c8a19ef809bca78bd58510156d6fd916d8fcdce8
Denis Pauk <pauk.denis@gmail.com>
2017-11-02 11:18:12 +0200

    Build cfy-autoscale by 'go build' instead 'make'

--- examples/cluster_blueprint/scripts/codes/scale-build.sh
+++ examples/cluster_blueprint/scripts/codes/scale-build.sh
@@ -19,12 +19,9 @@ mkdir -p /opt/cloudify-kubernetes-provider/src/k8s.io/autoscaler/cluster-autosca
 
 if [[ $? == 0 ]] && [[ -e "$AUTOSCALER_BINARY" ]]; then
     ctx logger info "Downloaded provided cluster-autoscaler"
-    cp $AUTOSCALER_BINARY /opt/cloudify-kubernetes-provider/src/k8s.io/autoscaler/cluster-autoscaler/
+    cp $AUTOSCALER_BINARY /opt/cloudify-kubernetes-provider/bin/cluster-autoscaler
 else
-    #ctx logger info "Build cluster-autoscaler from parent"
-    #cd /opt/cloudify-kubernetes-provider/
-    #go build -v -o bin/cluster-autoscaler src/k8s.io/autoscaler/cluster-autoscaler/main.go src/k8s.io/autoscaler/cluster-autoscaler/version.go
-    ctx logger info "Build cluster-autoscaler"
-    cd /opt/cloudify-kubernetes-provider/src/k8s.io/autoscaler/cluster-autoscaler/
-    make
+    ctx logger info "Build cluster-autoscaler from parent"
+    cd /opt/cloudify-kubernetes-provider/
+    go build -v -o bin/cluster-autoscaler src/k8s.io/autoscaler/cluster-autoscaler/main.go src/k8s.io/autoscaler/cluster-autoscaler/version.go
 fi
--- examples/cluster_blueprint/scripts/codes/scale-run.sh
+++ examples/cluster_blueprint/scripts/codes/scale-run.sh
@@ -1,5 +1,5 @@
 ctx logger info "Install cfy-autoscale provider"
-sudo cp /opt/cloudify-kubernetes-provider/src/k8s.io/autoscaler/cluster-autoscaler/cluster-autoscaler /usr/bin/cfy-autoscale
+sudo cp /opt/cloudify-kubernetes-provider/bin/cluster-autoscaler /usr/bin/cfy-autoscale
 sudo chmod 555 /usr/bin/cfy-autoscale
 sudo chown root:root /usr/bin/cfy-autoscale
 
