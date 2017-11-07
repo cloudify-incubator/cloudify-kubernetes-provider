07e4db8a9b2041ba6918ddf9623f69e9b4d926b9
earthmant <trammell@cloudify.co>
2017-11-07 21:54:20 +0200

    added a restart script to nodes

---
+++ examples/cluster_blueprint/scripts/kubernetes/node/start.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import subprocess
+from cloudify import ctx
+
+
+def execute_command(_command, extra_args=None):
+
+    ctx.logger.debug('_command {0}.'.format(_command))
+
+    subprocess_args = {
+        'args': _command.split(),
+        'stdout': subprocess.PIPE,
+        'stderr': subprocess.PIPE
+    }
+    if extra_args is not None and isinstance(extra_args, dict):
+        subprocess_args.update(extra_args)
+
+    ctx.logger.debug('subprocess_args {0}.'.format(subprocess_args))
+
+    process = subprocess.Popen(**subprocess_args)
+    output, error = process.communicate()
+
+    ctx.logger.debug('command: {0} '.format(_command))
+    ctx.logger.debug('output: {0} '.format(output))
+    ctx.logger.debug('error: {0} '.format(error))
+    ctx.logger.debug('process.returncode: {0} '.format(process.returncode))
+
+    if process.returncode:
+        ctx.logger.error('Running `{0}` returns error.'.format(_command))
+        return False
+
+    return output
+
+
+if __name__ == '__main__':
+
+    ctx.logger.info('Restarting Kubelet.')
+
+    execute_command('sudo systemctl restart kubelet')
