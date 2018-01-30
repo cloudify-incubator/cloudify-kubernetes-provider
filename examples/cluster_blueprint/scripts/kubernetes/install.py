cd4c0e03878c199293799ea85b8b961e6fc32051
Denis Pauk <pauk.denis@gmail.com>
2018-01-30 12:07:02 +0200

    flake8 + update cfy_kubernetes install script

--- examples/cluster_blueprint/scripts/kubernetes/install.py
+++ examples/cluster_blueprint/scripts/kubernetes/install.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python
+#
+# Copyright (c) 2017 GigaSpaces Technologies Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 import platform
 import subprocess
 from cloudify import ctx
@@ -64,7 +80,8 @@ if __name__ == '__main__':
                 'Failed to disable selinux')
 
         kubernetes_install = execute_command(
-            'sudo yum -y install kubeadm-1.8.4-0.x86_64 kubelet-1.8.4-0.x86_64 kubectl-1.8.4-0.x86_64'
+            'sudo yum -y install kubeadm-1.8.4-0.x86_64 '
+            'kubelet-1.8.4-0.x86_64 kubectl-1.8.4-0.x86_64'
         )
         if kubernetes_install is False:
             raise OperationRetry(
